7f411fab4201bbfa5cde7e09d49b2c2a6fd9edc7
Nora Benedict <ncb3ka@virginia.edu>
2018-02-28 14:42:13 -0500

    updated local settings

--- ocampo/ocampo/settings.py
+++ ocampo/ocampo/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = '/static/'
 # local_settings has full access to everything defined in this module.
 # Also force into sys.modules so it's visible to Django's autoreload.
 
-f = os.path.join(BASE_DIR, "local_settings.py")
+f = os.path.join(BASE_DIR, "ocampo", "local_settings.py")
 if os.path.exists(f):
     import sys
     import imp
@@ -130,4 +130,3 @@ if os.path.exists(f):
     module.__file__ = f
     sys.modules[module_name] = module
     exec(open(f, "rb").read())
-
